6586b17803e3192b5f51b2e6e93205d271a9d7d5
枫叶秋林 <80571808+liuqianpan2008@users.noreply.github.com>
2022-05-26 12:29:23 +0800

    新增个人信息

--- server/index.js
+++ server/index.js
@@ -43,6 +43,22 @@ wss.on("connection", (ws) => {
       }
       bot.chat(res.date ?? "");
     }
+    if (res.tape == "info") {
+      if (!bot) {
+        seedMessage.tape = "err";
+        seedMessage.mag = "你还没有登录";
+        ws.send(JSON.stringify(seedMessage));
+        return;
+      }
+      seedMessage.tape = "info";
+      seedMessage.mag = "查询信息成功";
+      seedMessage.date = {
+        health: bot.health,
+        food: bot.food,
+        oxy: bot.oxygenLevel,
+      };
+      ws.send(JSON.stringify(seedMessage));
+    }
     if (res.tape == "create") {
       const util = require("minecraft-server-util");
       if (res.date.port) {
@@ -70,7 +86,6 @@ wss.on("connection", (ws) => {
         console.log("发出数据===>\n" + JSON.stringify(seedMessage));
         ws.send(JSON.stringify(seedMessage));
       });
-
       bot?.on("chat", (username, message) => {
         seedMessage.tape = "chat";
         seedMessage.mag = "收到消息";
@@ -78,16 +93,28 @@ wss.on("connection", (ws) => {
         console.log("发出数据===>\n" + JSON.stringify(seedMessage));
         ws.send(JSON.stringify(seedMessage));
       });
-      /*
-      bot.on("playerJoined", (player) => {
-        if (player == bot) return;
-        seedMessage.tape = "playerJoined";
-        seedMessage.mag = "玩家加入";
-        seedMessage.date = player.displayName;
-        console.log("发出数据===>\n" + JSON.stringify(seedMessage));
+      // 血量，食物，氧气的检测！
+      bot?.on("health", () => {
+        seedMessage.tape = "info";
+        seedMessage.mag = "查询信息成功";
+        seedMessage.date = {
+          health: bot.health,
+          food: bot.food,
+          oxy: bot.oxygenLevel,
+        };
         ws.send(JSON.stringify(seedMessage));
       });
-			*/
+
+      bot?.on("breath", () => {
+        seedMessage.tape = "info";
+        seedMessage.mag = "查询信息成功";
+        seedMessage.date = {
+          health: bot.health,
+          food: bot.food,
+          oxy: bot.oxygenLevel,
+        };
+        ws.send(JSON.stringify(seedMessage));
+      });
       bot?.on("kicked", (reason) => {
         seedMessage.tape = "kicked";
         seedMessage.mag = "服务器被踢出";
--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,145 @@
 <template>
-
-  <n-layout>
-    <n-layout-header bordered
-                     style="min-height: 30px;"
-                     class="t">
-      <n-space justify="space-between">
-        <n-gradient-text gradient="linear-gradient(90deg, red 0%, green 50%, blue 100%)"
-                         :size="20">
-          我的世界聊天器 by-枫叶秋林
-        </n-gradient-text>
-        <n-space justify="end"
-                 class="nt">
-          <n-button type="success"
-                    @click="showModal = true"
-                    :disabled="setupdisabled">
-            设置
-          </n-button>
-          <n-modal v-model:show="showModal"
-                   class="custom-card"
-                   preset="card"
-                   style="width: '600px'"
-                   title="设置"
-                   size="huge"
-                   :bordered="false"
-                   :segmented="segmented">
-            <n-form ref="formRef"
-                    :model="formValue"
-                    :rules="rules"
-                    label-placement="left">
-              <n-form-item label="通信地址"
-                           path="ws">
-                <n-input v-model:value="formValue.ws"
-                         placeholder="通信地址" />
-              </n-form-item>
-              <n-form-item label="服务器地址"
-                           path="host">
-                <n-input v-model:value="formValue.host"
-                         placeholder="请输入地址" />
-              </n-form-item>
-              <n-form-item label="服务器端口"
-                           path="port">
-                <n-input v-model:value="formValue.port"
-                         placeholder="请输入端口" />
-              </n-form-item>
-              <n-form-item label="正版账户"
-                           path="username">
-                <n-input v-model:value="formValue.username"
-                         placeholder="请输入账号" />
-              </n-form-item>
-              <n-form-item label="正版密码"
-                           path="password">
-                <n-input v-model:value="formValue.password"
-                         type="password"
-                         placeholder="请输入密码" />
-              </n-form-item>
-              <n-form-item>
-                <n-button @click="lj"
-                          :loading="ljloading"
-                          :disabled="ljloading">连接</n-button>
-              </n-form-item>
-            </n-form>
-          </n-modal>
-          <n-button type="error"
-                    @click="quit">
-            关闭
-          </n-button>
-        </n-space>
-      </n-space>
-    </n-layout-header>
-    <n-layout-content bordered
-                      content-style="min-height: 340px;"
-                      :native-scrollbar="false">
-      <n-card style="height: 390px">
-        <n-space vertical>
-          <n-card v-for="(mag,i) in mags"
-                  :key="i"
-                  :title="mag.username">
-            <template #header-extra>
-              <n-time :time="mag.time" />
-            </template>
-            {{mag.char}}
-          </n-card>
-        </n-space>
-
-      </n-card>
-    </n-layout-content>
-    <n-layout-footer bordered
-                     style="min-height: 150px;">
-      <n-card>
-        <n-space vertical>
-          <n-input v-model:value="seedchar"
-                   type="textarea"
-                   placeholder="请输入内容" />
-          <n-space justify="end">
+  <div style="height: 598px; position: relative">
+    <n-layout sider-placement="right"
+              position="absolute">
+      <n-layout-header bordered
+                       style="height: 64px; padding: 20px"
+                       class="t">
+        <n-space justify="space-between">
+          <div>
+            <n-gradient-text gradient="linear-gradient(90deg, red 0%, green 50%, blue 100%)"
+                             :size="20">
+              我的世界聊天器 by-枫叶秋林
+            </n-gradient-text>
+            状态:{{setupdisabled?"已连接":"未连接"}}
+            账号：{{formValue.username??"无"}}
+            服务器:{{formValue.host??"无"}}
+          </div>
+          <n-space justify="end"
+                   class="nt">
             <n-button type="success"
-                      @click="seedc"
-                      :disabled="!setupdisabled">
-              发送
+                      @click="showModal = true"
+                      :disabled="setupdisabled">
+              设置
+            </n-button>
+            <n-modal v-model:show="showModal"
+                     class="custom-card"
+                     preset="card"
+                     style="width: '600px'"
+                     title="设置"
+                     size="huge"
+                     :bordered="false"
+                     :segmented="segmented">
+              <n-form ref="formRef"
+                      :model="formValue"
+                      :rules="rules"
+                      label-placement="left">
+                <n-form-item label="通信地址"
+                             path="ws">
+                  <n-input v-model:value="formValue.ws"
+                           placeholder="通信地址" />
+                </n-form-item>
+                <n-form-item label="服务器地址"
+                             path="host">
+                  <n-input v-model:value="formValue.host"
+                           placeholder="请输入地址" />
+                </n-form-item>
+                <n-form-item label="服务器端口"
+                             path="port">
+                  <n-input v-model:value="formValue.port"
+                           placeholder="请输入端口" />
+                </n-form-item>
+                <n-form-item label="正版账户"
+                             path="username">
+                  <n-input v-model:value="formValue.username"
+                           placeholder="请输入账号" />
+                </n-form-item>
+                <n-form-item label="正版密码"
+                             path="password">
+                  <n-input v-model:value="formValue.password"
+                           type="password"
+                           placeholder="请输入密码" />
+                </n-form-item>
+                <n-form-item>
+                  <n-button @click="lj"
+                            :loading="ljloading"
+                            :disabled="ljloading">连接</n-button>
+                </n-form-item>
+              </n-form>
+            </n-modal>
+            <n-button type="error"
+                      @click="quit">
+              关闭
             </n-button>
           </n-space>
         </n-space>
-      </n-card>
-    </n-layout-footer>
-  </n-layout>
+      </n-layout-header>
+      <n-layout has-sider
+                position="absolute"
+                style="top: 60px; bottom: 145px">
+        <!-- 侧边栏 -->
+        <n-layout-sider collapse-mode="transform"
+                        :collapsed-width="15"
+                        :width="240"
+                        show-trigger="arrow-circle"
+                        content-style="padding: 24px;"
+                        bordered>
+          <n-card title="个人信息">
+            饱食度：{{info.food}}<br>
+            生命力：{{info.health}}<br>
+            <!--疑似BUG 氧气:{{info.oxy}} -->
+          </n-card>
+        </n-layout-sider>
+        <n-layout-content bordered
+                          :native-scrollbar="false">
+          <n-card hoverable>
+            <n-space vertical>
+              <n-card v-for="(mag,i) in mags"
+                      :key="i"
+                      :title="mag.username"
+                      :header-style="mag.username=='系统'?'color: red':''">
+                <template #header-extra>
+                  <n-time :time="mag.time" />
+                </template>
+                {{mag.char}}
+              </n-card>
+            </n-space>
+          </n-card>
 
+        </n-layout-content>
+      </n-layout>
+      <!-- 发送区 -->
+      <n-layout-footer bordered
+                       style="height: 145px;"
+                       position="absolute">
+        <n-card>
+          <n-space vertical>
+            <n-input v-model:value="seedchar"
+                     type="textarea"
+                     placeholder="请输入内容" />
+            <n-space justify="end">
+              <n-button type="success"
+                        @click="quitWS"
+                        :disabled="!setupdisabled">
+                关闭
+              </n-button>
+              <n-button type="success"
+                        @click="seedc"
+                        :disabled="!setupdisabled">
+                发送
+              </n-button>
+            </n-space>
+          </n-space>
+        </n-card>
+      </n-layout-footer>
+    </n-layout>
+  </div>
 </template>
 
 <script setup>
 import { ref } from "@vue/reactivity";
-//import { ipcRenderer } from 'electron';
 const { ipcRenderer } = window.require('electron')
-
+const quitWS = ref()
 const setupdisabled = ref(false)
 const showModal = ref(false)
 const ljloading = ref(false)
@@ -125,7 +154,7 @@ const formValue = ref({
   password: ""
 })
 const mags = ref([{
-  username: "欢迎使用",
+  username: "系统",
   char: "请点击右上角设置！",
   time: new Date()
 }])
@@ -146,6 +175,11 @@ const rules = {
     trigger: 'blur'
   },
 }
+const info = ref({
+  health: 0,
+  food: 0,
+  oxy: 0,
+})
 // 连接按钮事件
 const lj = () => {
   formRef.value?.validate((errors) => {
@@ -159,7 +193,9 @@ const lj = () => {
         mag: "",
       };
       WS.addEventListener('message', function (e) {
-        console.log("收到信息===>" + e.data);
+        if (e.tape != "info") {
+          console.log("收到信息===>" + e.data);
+        }
         const res = JSON.parse(e.data)
         if (res.tape == "chat") {
           mags.value.push({
@@ -174,23 +210,29 @@ const lj = () => {
             setupdisabled.value = true;
             showModal.value = false;
           }
+          if (res.tape == "info") {
+            info.value = res.date
+            return;
+          }
           mags.value.push({
-            username: "系统:" + res.mag,
-            char: res.date ?? res.mag,
+            username: "系统",
+            char: res.mag,
             time: new Date()
           })
         }
       }, false)
       WS.addEventListener('open', function (e) {
         console.log("WS open", e);
+        // 服务器登录
         seedMessage.tape = "create"
         seedMessage.date = {
           host: formValue.value.host,
           port: formValue.value.port,
-          username: formValue.value.username, // minecraft username
+          username: formValue.value.username,
           password: formValue.value.password,
         }
         WS.send(JSON.stringify(seedMessage))
+        //聊天信息发送
         seedc.value = () => {
           seedMessage.tape = "chat"
           seedMessage.date = seedchar.value
@@ -200,11 +242,11 @@ const lj = () => {
         }
       }, false)
       WS.addEventListener('close', function (e) {
-        console.log("WS close", e);
+        console.log("通信关闭" + e.message);
         ljloading.value = false;
       }, false)
       WS.addEventListener('error', function (e) {
-        console.log("WS error", e);
+        console.log("通信错误" + e.message);
         ljloading.value = false;
       }, false)
     }
